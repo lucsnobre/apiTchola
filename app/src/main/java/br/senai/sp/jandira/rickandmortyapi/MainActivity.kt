ecc441fdbe2e41365b74f329ebe4a2d161cf5075
LUCAS RODRIGUES NOBRE SILVA <24122499@dsn127m1241954.cfp127aluno.local>
2025-05-13 14:55:47 -0300

    piroca finalizada

--- app/src/main/java/br/senai/sp/jandira/rickandmortyapi/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/rickandmortyapi/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import br.senai.sp.jandira.rickandmortyapi.screens.HomeScreen
 import br.senai.sp.jandira.rickandmortyapi.ui.theme.RickAndMortyAPITheme
 
 class MainActivity : ComponentActivity() {
@@ -19,12 +20,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             RickAndMortyAPITheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                HomeScreen()
             }
         }
     }
