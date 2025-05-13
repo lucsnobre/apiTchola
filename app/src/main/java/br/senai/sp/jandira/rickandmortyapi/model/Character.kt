65b709d8275d9a1aca42300c2230877f5239ca95
LUCAS RODRIGUES NOBRE SILVA <24122499@dsn127m1241954.cfp127aluno.local>
2025-05-13 11:30:45 -0300

    kotlennigga

---
+++ app/src/main/java/br/senai/sp/jandira/rickandmortyapi/model/Character.kt
@@ -0,0 +1,6 @@
+package br.senai.sp.jandira.rickandmortyapi.model
+
+data class Character {
+    var id: Int = 0,
+    var name: String = ""
+}
---
+++ app/src/main/java/br/senai/sp/jandira/rickandmortyapi/model/Result.kt
@@ -0,0 +1,5 @@
+package br.senai.sp.jandira.rickandmortyapi.model
+
+data class Result(
+    var results: List<Character>? = null
+)
