5adb701902a11579f92dd90504bb2d28c53c4007
LUCAS RODRIGUES NOBRE SILVA <24122499@dsn127m1241954.cfp127aluno.local>
2025-05-13 13:42:15 -0300

    so nas do job

--- app/src/main/java/br/senai/sp/jandira/rickandmortyapi/model/Character.kt
+++ app/src/main/java/br/senai/sp/jandira/rickandmortyapi/model/Character.kt
@@ -1,6 +1,9 @@
 package br.senai.sp.jandira.rickandmortyapi.model
 
-data class Character {
+data class Character (
     var id: Int = 0,
-    var name: String = ""
-}
+    var name: String = "",
+    var status: String = "",
+    var species: String = "",
+    var images: String = ""
+)
